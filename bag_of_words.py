791d359b1b6fa3617c6e6fdcc67f0bac4729cf9c
Michael Mason <michael.mason@numerica.us>
2018-12-02 16:34:38 -0700

    steal better bow bag_of_words.py

--- bag_of_words.py
+++ bag_of_words.py
@@ -1,5 +1,20 @@
+import string
+
+import os
+import json
+import string
 import numpy as np
 import pandas as pd
+from pandas.io.json import json_normalize
+import matplotlib.pyplot as plt
+import seaborn as sns
+color = sns.color_palette()
+import eli5
+
+
+from sklearn import model_selection, preprocessing, metrics, ensemble, naive_bayes, linear_model
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.decomposition import TruncatedSVD
 
 from sklearn.model_selection import train_test_split
 
@@ -12,23 +27,76 @@ from sklearn.metrics import f1_score
 
 from sklearn.pipeline import Pipeline
 
+from wordcloud import WordCloud, STOPWORDS
+
+
 train_df = pd.read_csv("../input/train.csv")
-train_df, val_df = train_test_split(train_df, test_size=0.1, random_state=12378)
+test_df = pd.read_csv("../input/test.csv")
+print("Train shape : ", train_df.shape)
+print("Test shape : ", test_df.shape)
 
 
+## Number of words in the text ##
+train_df["num_words"] = train_df["question_text"].apply(lambda x: len(str(x).split()))
+test_df["num_words"] = test_df["question_text"].apply(lambda x: len(str(x).split()))
 
-from sklearn.linear_model import SGDClassifier
-text_clf = Pipeline([
-    ('vect', CountVectorizer()),
-    ('tfidf', TfidfTransformer()),
-    ('reg', LogisticRegression())
-])
+## Number of unique words in the text ##
+train_df["num_unique_words"] = train_df["question_text"].apply(lambda x: len(set(str(x).split())))
+test_df["num_unique_words"] = test_df["question_text"].apply(lambda x: len(set(str(x).split())))
 
-text_clf.fit(train_df['question_text'], train_df['target'])
+## Number of characters in the text ##
+train_df["num_chars"] = train_df["question_text"].apply(lambda x: len(str(x)))
+test_df["num_chars"] = test_df["question_text"].apply(lambda x: len(str(x)))
 
-predicted = text_clf.predict(val_df['question_text'])
-for thresh in [0.5]:
+## Number of stopwords in the text ##
+train_df["num_stopwords"] = train_df["question_text"].apply(lambda x: len([w for w in str(x).lower().split() if w in STOPWORDS]))
+test_df["num_stopwords"] = test_df["question_text"].apply(lambda x: len([w for w in str(x).lower().split() if w in STOPWORDS]))
+
+## Number of punctuations in the text ##
+train_df["num_punctuations"] =train_df['question_text'].apply(lambda x: len([c for c in str(x) if c in string.punctuation]) )
+test_df["num_punctuations"] =test_df['question_text'].apply(lambda x: len([c for c in str(x) if c in string.punctuation]) )
+
+## Number of title case words in the text ##
+train_df["num_words_upper"] = train_df["question_text"].apply(lambda x: len([w for w in str(x).split() if w.isupper()]))
+test_df["num_words_upper"] = test_df["question_text"].apply(lambda x: len([w for w in str(x).split() if w.isupper()]))
+
+## Number of title case words in the text ##
+train_df["num_words_title"] = train_df["question_text"].apply(lambda x: len([w for w in str(x).split() if w.istitle()]))
+test_df["num_words_title"] = test_df["question_text"].apply(lambda x: len([w for w in str(x).split() if w.istitle()]))
+
+## Average length of the words in the text ##
+train_df["mean_word_len"] = train_df["question_text"].apply(lambda x: np.mean([len(w) for w in str(x).split()]))
+test_df["mean_word_len"] = test_df["question_text"].apply(lambda x: np.mean([len(w) for w in str(x).split()]))
+
+# Get the tfidf vectors #
+tfidf_vec = TfidfVectorizer(stop_words='english', ngram_range=(1,3))
+tfidf_vec.fit_transform(train_df['question_text'].values.tolist() + test_df['question_text'].values.tolist())
+train_tfidf = tfidf_vec.transform(train_df['question_text'].values.tolist())
+test_tfidf = tfidf_vec.transform(test_df['question_text'].values.tolist())
+train_y = train_df["target"].values
+
+def runModel(train_X, train_y, test_X, test_y, test_X2):
+    model = linear_model.LogisticRegression(C=5., solver='sag')
+    model.fit(train_X, train_y)
+    pred_test_y = model.predict_proba(test_X)[:,1]
+    pred_test_y2 = model.predict_proba(test_X2)[:,1]
+    return pred_test_y, pred_test_y2, model
+
+print("Building model.")
+cv_scores = []
+pred_full_test = 0
+pred_train = np.zeros([train_df.shape[0]])
+kf = model_selection.KFold(n_splits=5, shuffle=True, random_state=2017)
+for dev_index, val_index in kf.split(train_df):
+    dev_X, val_X = train_tfidf[dev_index], train_tfidf[val_index]
+    dev_y, val_y = train_y[dev_index], train_y[val_index]
+    pred_val_y, pred_test_y, model = runModel(dev_X, dev_y, val_X, val_y, test_tfidf)
+    pred_full_test = pred_full_test + pred_test_y
+    pred_train[val_index] = pred_val_y
+    cv_scores.append(metrics.log_loss(val_y, pred_val_y))
+    break
+
+for thresh in np.arange(0.1, 0.201, 0.01):
     thresh = np.round(thresh, 2)
-    print("F1 score at threshold {0} is {1}".format(thresh, f1_score(val_df['target'], (predicted>thresh).astype(int))))
-
-print("Baseline F1 score: ", f1_score(val_df['target'], np.ones(val_df['target'].size).astype(int)))
+    print("F1 score at threshold {0} is {1}".format(thresh, metrics.f1_score(val_y, (pred_val_y>thresh).astype(int))))
+eli5.show_weights(model, vec=tfidf_vec, top=100, feature_filter=lambda x: x != '<BIAS>')
